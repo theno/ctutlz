4b72675dfd3b5c09c065006d272b75fd278e35e3
Theodor Nolte <theno@gmx.net>
2017-03-17 09:03:28 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ long_description = create_readme_with_long_description()
 
 setup(
     name='ctutlz',
-    version='0.1.0',
+    version='0.2.0',
     description=description,
     long_description=long_description,
     url='https://github.com/theno/ctutlz',
