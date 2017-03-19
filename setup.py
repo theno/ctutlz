3299460a257a76368f85be52e374fc08557a2c2d
Theodor Nolte <theno@gmx.net>
2017-03-19 19:18:08 +0100

    Use forked pyOpenSSL-16.3.0

--- setup.py
+++ setup.py
@@ -82,7 +82,8 @@ setup(
         #'https://github.com/etingof/pyasn1-modules/tarball/master#egg=pyasn1-modules-0.0.9',
         'git+https://github.com/etingof/pyasn1-modules.git#egg=pyasn1-modules-0.0.9',
         # 16.2.0 has no ocsp support
-        'git+https://github.com/pyca/pyopenssl.git#egg=pyOpenSSL-16.3.0',
+        # 'git+https://github.com/pyca/pyopenssl.git#egg=pyOpenSSL-16.3.0',
+        'git+https://github.com/theno/pyopenssl.git#egg=pyOpenSSL-16.3.0',
     ],
     extras_require={
         'dev': ['pypandoc'],
