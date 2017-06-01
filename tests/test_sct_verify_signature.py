ddf1a31eccf7bf12e6726de348623a5d4a7be7d4
Theodor Nolte <theno@gmx.net>
2017-06-01 09:36:25 +0200

    remove obsolete unit test

--- tests/test_sct_verify_signature.py
+++ tests/test_sct_verify_signature.py
@@ -2,32 +2,7 @@ from os.path import join, dirname
 
 from utlz import flo
 
-from ctutlz.sct.verification import verify_signature_CLUNKY, verify_signature
-
-
-def test_verify_signature_clunky():
-    basedir = join(dirname(__file__), 'data', 'test_sct_verify_signature')
-
-    signature_input = \
-        open(flo('{basedir}/signature_input_valid.bin'), 'rb').read()
-    signature = open(flo('{basedir}/signature.der'), 'rb').read()
-    pubkey = open(flo('{basedir}/pubkey.pem'), 'rb').read()
-
-    got_verified, got_output, got_cmd_res = \
-        verify_signature_CLUNKY(signature_input, signature, pubkey)
-
-    assert got_verified is True
-    assert got_output == 'Verified OK\n'
-    assert got_cmd_res.exitcode == 0
-
-    signature_input = b'some invalid signature input'
-
-    got_verified, got_output, got_cmd_res = \
-        verify_signature_CLUNKY(signature_input, signature, pubkey)
-
-    assert got_verified is False
-    assert got_output == 'Verification Failure\n'
-    assert got_cmd_res.exitcode == 1
+from ctutlz.sct.verification import verify_signature
 
 
 def test_verify_signature():
