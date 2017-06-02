da8f18e56cd2035e69781234df4aa72c09dd1f47
Theodor Nolte <theno@gmx.net>
2017-06-02 08:29:47 +0200

    add flags to switch on/off callbacks for tls and ocsp to grab scts

--- ctutlz/tls/handshake.py
+++ ctutlz/tls/handshake.py
@@ -6,91 +6,106 @@ import struct
 from functools import reduce
 
 import certifi
+import OpenSSL
 import pyasn1_modules.rfc5280
 from pyasn1.codec.der.decoder import decode as der_decoder
 from pyasn1.type.univ import OctetString
-from OpenSSL import SSL, crypto
 from utlz import flo
 
 from ctutlz.sct.sct import Sct
 from ctutlz.tls.sctlist import SignedCertificateTimestampList
 
 
-def create_context():
+def create_context(scts_tls, scts_ocsp):
+    '''
+    Args:
+        scts_tls: If True, register callback for TSL extension 18 (for SCTs)
+        scts_ocsp: If True, register callback for OCSP-response (for SCTs)
+    '''
 
     def verify_callback(conn, cert, errnum, depth, ok):
         '''Dummy callback.'''
         return 1  # True
 
-    ctx = SSL.Context(SSL.SSLv23_METHOD)
-    ctx.set_options(SSL.OP_NO_SSLv2)
-    ctx.set_options(SSL.OP_NO_SSLv3)
+    ctx = OpenSSL.SSL.Context(OpenSSL.SSL.SSLv23_METHOD)
+    ctx.set_options(OpenSSL.SSL.OP_NO_SSLv2)
+    ctx.set_options(OpenSSL.SSL.OP_NO_SSLv3)
 
-    ctx.set_verify(SSL.VERIFY_PEER, verify_callback)
+    ctx.set_verify(OpenSSL.SSL.VERIFY_PEER, verify_callback)
     ca_filename = certifi.where()
     ctx.load_verify_locations(ca_filename)
 
-    # TLS extension 18
-
     ctx.tls_extension_18_tdf = None
+    if scts_tls:
+        from ctutlz.tls.handshake_openssl import ffi, lib
 
-    from ctutlz.tls.handshake_openssl import ffi, lib
+        # this annotation makes the callback function available at
+        # lib.serverinfo_cli_parse_cb() of type cdef so it can be used as
+        # argument for the call of lib.SSL_CTX_add_client_custom_ext()
+        @ffi.def_extern()
+        def serverinfo_cli_parse_cb(ssl, ext_type, _in, inlen, al, arg):
+            if ext_type == 18:
 
-    # this annotation makes the callback function available at
-    # lib.serverinfo_cli_parse_cb() of type cdef so it can be used as argument
-    # for the call of lib.SSL_CTX_add_client_custom_ext()
-    @ffi.def_extern()
-    def serverinfo_cli_parse_cb(ssl, ext_type, _in, inlen, al, arg):
-        if ext_type == 18:
+                def reduce_func(accum_value, current):
+                    fmt = accum_value[0] + current[0]
+                    values = accum_value[1] + (current[1], )
+                    return fmt, values
 
-            def reduce_func(accum_value, current):
-                fmt = accum_value[0] + current[0]
-                values = accum_value[1] + (current[1], )
-                return fmt, values
+                initializer = ('!', ())
+                fmt, values = reduce(reduce_func, [
+                    ('H', ext_type),
+                    ('H', inlen),
+                    (flo('{inlen}s'), bytes(ffi.buffer(_in, inlen))),
+                ], initializer)
+                ctx.tls_extension_18_tdf = struct.pack(fmt, *values)
+            return 1  # True
 
-            initializer = ('!', ())
-            fmt, values = reduce(reduce_func, [
-                ('H', ext_type),
-                ('H', inlen),
-                (flo('{inlen}s'), bytes(ffi.buffer(_in, inlen))),
-            ], initializer)
-            ctx.tls_extension_18_tdf = struct.pack(fmt, *values)
-        return 1  # True
-
-    # register callback for TLS extension result into the SSL context created
-    # with PyOpenSSL, using OpenSSL "directly"
-    if not lib.SSL_CTX_add_client_custom_ext(ffi.cast('struct ssl_ctx_st *',
-                                                      ctx._context),
-                                             18,
-                                             ffi.NULL, ffi.NULL, ffi.NULL,
-                                             lib.serverinfo_cli_parse_cb,
-                                             ffi.NULL):
-        import sys
-        sys.stderr.write('Unable to add custom extension 18\n')
-        lib.ERR_print_errors_fp(sys.stderr)
-        sys.exit(1)
-
-    # OCSP
+        # register callback for TLS extension result into the SSL context
+        # created with PyOpenSSL, using OpenSSL "directly"
+        if not lib.SSL_CTX_add_client_custom_ext(ffi.cast('struct ssl_ctx_st *',
+                                                          ctx._context),
+                                                 18,
+                                                 ffi.NULL, ffi.NULL, ffi.NULL,
+                                                 lib.serverinfo_cli_parse_cb,
+                                                 ffi.NULL):
+            import sys
+            sys.stderr.write('Unable to add custom extension 18\n')
+            lib.ERR_print_errors_fp(sys.stderr)
+            sys.exit(1)
 
     ctx.ocsp_resps = []
+    if scts_ocsp:
 
-    def ocsp_client_callback(connection, ocsp_data, data):
-        ctx.ocsp_resps.append(ocsp_data)
-        return True
+        def ocsp_client_callback(connection, ocsp_data, data):
+            ctx.ocsp_resps.append(ocsp_data)
+            return True
 
-    ctx.set_ocsp_client_callback(ocsp_client_callback, data=None)
+        ctx.set_ocsp_client_callback(ocsp_client_callback, data=None)
 
     return ctx
 
 
-def create_socket():
-    ctx = create_context()
+def create_socket(scts_tls, scts_ocsp):
+    '''
+    Args:
+        scts_tls: If True, register callback for TSL extension 18 (for SCTs)
+        scts_ocsp: If True, register callback for OCSP-response (for SCTs)
+    '''
+    ctx = create_context(scts_tls, scts_ocsp)
     raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    return SSL.Connection(ctx, raw_sock)
+    return OpenSSL.SSL.Connection(ctx, raw_sock)
 
 
-def do_handshake(domain):
-    sock = create_socket()
+# formerly cert_of_domain
+def do_handshake(domain, scts_tls, scts_ocsp):
+    '''
+    Args:
+        domain: string with domain name,
+                for example: 'ritter.vg', or 'www.ritter.vg'
+        scts_tls: If True, register callback for TSL extension 18 (for SCTs)
+        scts_ocsp: If True, register callback for OCSP-response (for SCTs)
+    '''
+    sock = create_socket(scts_tls, scts_ocsp)
 
     sock.request_ocsp()
 
@@ -104,11 +119,12 @@ def do_handshake(domain):
         certificate = sock.get_peer_certificate()
 
         ctx = sock.get_context()
-        if ctx.ocsp_resps:
-            ocsp_response = ctx.ocsp_resps[0]
-
-        if ctx.tls_extension_18_tdf:
-            tls_extension_18_tdf = ctx.tls_extension_18_tdf
+        if scts_tls:
+            if ctx.tls_extension_18_tdf:
+                tls_extension_18_tdf = ctx.tls_extension_18_tdf
+        if scts_ocsp:
+            if ctx.ocsp_resps:
+                ocsp_response = ctx.ocsp_resps[0]
 
     except Exception as exc:
         import traceback
@@ -122,14 +138,23 @@ def do_handshake(domain):
     return certificate, chain, ocsp_response, tls_extension_18_tdf
 
 
-def cert_of_domain(domain):
-    cert_x509, chain_x509s, ocsp_resp_der, tls_ext_18_tdf = do_handshake(domain)
-    cert_der = crypto.dump_certificate(type=crypto.FILETYPE_ASN1,
-                                       cert=cert_x509)
+def cert_of_domain(domain, scts_tls=True, scts_ocsp=True):
+    '''
+    Args:
+        domain: string with domain name,
+                for example: 'ritter.vg', or 'www.ritter.vg'
+        scts_tls: If True, register callback for TSL extension 18 (for SCTs)
+        scts_ocsp: If True, register callback for OCSP-response (for SCTs)
+    '''
+    cert_x509, chain_x509s, ocsp_resp_der, tls_ext_18_tdf = do_handshake(
+        domain, scts_tls, scts_ocsp)
+
+    cert_der = OpenSSL.crypto.dump_certificate(
+        type=OpenSSL.crypto.FILETYPE_ASN1, cert=cert_x509)
     # https://tools.ietf.org/html/rfc5246#section-7.4.2
     issuer_cert_x509 = chain_x509s[1]
-    issuer_cert_der = crypto.dump_certificate(type=crypto.FILETYPE_ASN1,
-                                              cert=issuer_cert_x509)
+    issuer_cert_der = OpenSSL.crypto.dump_certificate(
+        type=OpenSSL.crypto.FILETYPE_ASN1, cert=issuer_cert_x509)
     return cert_der, issuer_cert_der, ocsp_resp_der, tls_ext_18_tdf
 
 
