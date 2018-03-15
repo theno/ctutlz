9aae5e122ac268ec43bb43d6fac6ce1db4bb467e
Theodor Nolte <theno@gmx.net>
2018-03-15 14:58:32 +0100

    refactor test_verify_signature() and add test data

--- tests/test_sct_verify_signature.py
+++ tests/test_sct_verify_signature.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from os.path import join, dirname
 
 from utlz import flo
@@ -5,16 +6,58 @@ from utlz import flo
 from ctutlz.sct.verification import verify_signature
 
 
-def test_verify_signature():
+Item = namedtuple(
+    typename='TestItem',
+    field_names=[
+        'domain',
+        'pubkey_pem',
+        'signature_der',
+        'signature_input_bin',
+        'expected_verify',
+    ])
+
+
+def from_file(filename):
     basedir = join(dirname(__file__), 'data', 'test_sct_verify_signature')
+    with open(flo('{basedir}/{filename}'), 'rb') as fh:
+        data = fh.read()
+    return data
 
-    signature_input = \
-        open(flo('{basedir}/signature_input_valid.bin'), 'rb').read()
-    signature = open(flo('{basedir}/signature.der'), 'rb').read()
-    pubkey = open(flo('{basedir}/pubkey.pem'), 'rb').read()
 
-    assert verify_signature(signature_input, signature, pubkey) is True
-
-    signature_input = b'some invalid signature input'
-
-    assert verify_signature(signature_input, signature, pubkey) is False
+def test_verify_signature():
+    test_data = [
+        Item(
+            domain='',
+            expected_verify=True,
+            signature_input_bin=from_file('signature_input_valid.bin'),
+            signature_der=from_file('signature.der'),
+            pubkey_pem=from_file('pubkey.pem')
+        ),
+        Item(
+            domain='',
+            expected_verify=False,
+            signature_input_bin=b'some invalid signature input',
+            signature_der=from_file('signature.der'),
+            pubkey_pem=from_file('pubkey.pem')
+        ),
+        Item(
+            domain='google.com',
+            expected_verify=True,
+            signature_input_bin=from_file('google.com/signature_input.bin'),
+            signature_der=from_file('google.com/signature.der'),
+            pubkey_pem=from_file('google.com/pubkey.pem')
+        ),
+        # Item(
+        #     domain='pirelli.com',
+        #     expected_verify=True,
+        #     signature_input_bin=from_file('pirelli.com/signature_input.bin'),
+        #     signature_der=from_file('pirelli.com/signature.der'),
+        #     pubkey_pem=from_file('pirelli.com/pubkey.pem')
+        # ),
+    ]
+    for item in test_data:
+        assert verify_signature(item.signature_input_bin,
+                                item.signature_der,
+                                item.pubkey_pem) \
+            is item.expected_verify, flo('verify_signature() for {item.domain} '
+                                         'must return {item.expected_verify}')
