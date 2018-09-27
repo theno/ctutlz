2b4dd79d0bd15844761636066c3cdd6d614cb627
Theodor Nolte <theno@gmx.net>
2018-09-27 21:32:58 +0200

    add Python-3.7 support

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='python development utilities library '
              'certificate-transparency ct signed-certificate-timestamp sct',
